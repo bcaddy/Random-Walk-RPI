b7b9e058dc68ee87197e6f6161741fb7c8e44726
taxicata <86631080+taxicata@users.noreply.github.com>
2021-06-30 12:30:54 -0500

    Update random-walk.py

--- original-code/random-walk.py
+++ original-code/random-walk.py
@@ -5,12 +5,14 @@ import random
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+from scipy.stats import norm
 
 sns.set_style('darkgrid')
 
   
 # defining the number of steps
-n = 400
+n = 1000   # nmber of runs
+m = 1000   # number of steps per run
 xfinal = np.zeros(n)
 yfinal = np.zeros(n)
 
@@ -18,37 +20,28 @@ yfinal = np.zeros(n)
 #creating two array for containing x and y coordinate
 #of size equals to the number of size and filled up with 0's
 
-  
-for k in range (1,400):
+for k in range (1,n):
     x = np.zeros(n)
     y = np.zeros(n)
     
     # filling the coordinates with random variables
-    for i in range(1, n):
+        
+    for i in range (1, m):
 
-        for j in range (1, i):
-            num = random.random()
-            if num >= 0 and num < 0.25:
-                val = 1
-            elif num >= 0.25 and num < 0.5:
-                val = 2
-            elif num >= 0.5 and num < 0.75:
-                val = 3
-            else:
-                val = 4
+        num = random.random()
 
-            if val == 1:
-                x[i] = x[i - 1] + 1
-                y[i] = y[i - 1]
-            elif val == 2:
-                x[i] = x[i - 1] - 1
-                y[i] = y[i - 1]
-            elif val == 3:
-                x[i] = x[i - 1]
-                y[i] = y[i - 1] + 1
-            else:
-                x[i] = x[i - 1]
-                y[i] = y[i - 1] - 1
+        if num >= 0 and num < 0.25:
+            x[i] = x[i - 1] + 1
+            y[i] = y[i - 1]
+        elif num >= 0.25 and num < 0.5:
+            x[i] = x[i - 1] - 1
+            y[i] = y[i - 1]
+        elif num >= 0.5 and num < 0.75:
+            x[i] = x[i - 1]
+            y[i] = y[i - 1] + 1
+        else:
+            x[i] = x[i - 1]
+            y[i] = y[i - 1] - 1
                 
     xfinal[k] = x[i]
     yfinal[k] = y[i]  
@@ -61,8 +54,8 @@ for k in range (1,400):
 
 # histogram and fit
 
-sns.histplot(xfinal, kde=True)
+sns.distplot(xfinal, fit=norm, kde=False)
 pylab.show()
 
-sns.histplot(yfinal, kde=True)
+sns.distplot(yfinal, fit=norm, kde=False)
 pylab.show()
