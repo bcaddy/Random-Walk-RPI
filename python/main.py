a05c750e984abbbac5a694d7b2b7ef247f67182b
zarebalt <dward656@gmail.com>
2021-07-12 14:53:14 -0700

    Bug Fixes for Bob's Sanity Just some quick fixes to preserve your sanity in translating the code. I fixed some mismatched variables and improper types, fixed a case issue in a function call, and added in some variables needed by the coordinate transforms. Also the main program name has been changed to eliminate confusion: recpetorwalk.py -> main.py. This also has some arrays to catch end point data.

--- python/main.py
+++ python/main.py
@@ -10,16 +10,16 @@ nantag=200    #number of walker antagonists
 nwalker=1000  #number of walkers
 mantag=1000   #number of steps for antagonists
 mwalker=1000  #        ""      for walkers
-axis_x = 2*m+1 #range of the axis for x and y
+axis_x = 2*(mwalker)+1 #range of the axis for x and y
 board = np.zeros((axis_x,axis_x)) #initialize the board
-boardfill=math.floor(np.random.normal(loc=((2*m+1)**2)/2,scale=mu)) #randomly samples Gaussian to determine number of receptors
-adest = np.array((0,2))
-antagseal = np.array((0,2))
+boardfill=math.floor(np.random.normal(loc=((2*(mwalker)+1)**2)/2,scale=mu)) #randomly samples Gaussian to determine number of receptors
+adest = np.array([[]])
+antagseal = np.array([[]])
+wsucceed = np.array([[]])
+wdest = np.array([[]])
 
-wdest = np.array((0,2))
 
-
-def simulation(nsteps, recarray, origin=[0,0], group=10, prob=[0.25,0.25,0.25,0.25]):
+def simulation(nsteps, axis, recarray, origin=[[0,0]], group=10, prob=[0.25,0.25,0.25,0.25]):
         
         # Does the actual random walk simulation.
         # The total number of steps to be taken by the walker is divided into groups of size group_size.
@@ -30,20 +30,20 @@ def simulation(nsteps, recarray, origin=[0,0], group=10, prob=[0.25,0.25,0.25,0.
         # the desired number of steps is reached, being nsteps
         bins=nsteps//group
         choices=[[1,0],[0,1],[0,-1],[-1,0]]
+        path=np.array(origin)
         for i in range(bins):
             steps = rng.choice(choices, size=group, p=prob)  # array of currently generated steps
-            add_on = steps.cumsum(0) + path[-1, 0:2]
+            add_on = steps.cumsum(0) + path[-1,:]
 
             for j in range(group):
-                check=Receptors.coordstoIndex(add_on[j])
+                check=Receptors.coordsToIndex(add_on[j],nsteps,axis)
                 if(recarray[check]):
                     reccoord = add_on[j,:]  # receptor that walker intercepted with
                     fadd_on = add_on[:j+1,:]
                     path = np.concatenate([path, fadd_on])
                     return(path, reccoord)
                 else:
-                    continue 
-            path = np.concatenate([path, add_on])
+                    path = np.concatenate([path, add_on])
             
         reccoord = None  # receptor set to None if walker does not intercept any during simulation
         return(path, reccoord)
@@ -52,12 +52,16 @@ def simulation(nsteps, recarray, origin=[0,0], group=10, prob=[0.25,0.25,0.25,0.
 recarray,board=Receptors.seedReceptors(board,boardfill)
 #Antagonists run the board
 for i in range(nantag):
-    history,reccoord = simulation(mantag,recarray)
+    history,reccoord = simulation(mantag,axis_x,recarray)
     if(reccoord is not None):
         recarray = Receptors.sealPortal(reccoord,mantag,axis_x,recarray)
-        antagseal[i,:] = 
+        antagseal[i,:].append(history[-1,:],axis=0)
     else:
         adest.append(history[-1,:],axis=0)
 #Now we let the walkers run the board
 for i in range(nwalker):
-    history,reccoord = simulation(mwalker,recarray)
+    history,reccoord = simulation(mwalker,axis_x,recarray)
+    if(reccoord is not None):
+        wsucceed.append(history[-1,:],axis=0)
+    else:
+        wdest.append(history[-1,:],axis=0)
